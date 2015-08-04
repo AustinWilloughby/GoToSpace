373db8566de4d51ed9fd075b7f4f6a71e1a95582
rck1464 <rck1464@rit.edu>
2015-08-04 18:06:05 -0400

    Placeholders, Zooming, Fading, Clouds
    Placeholder sprites added. Camera zooms to wall when clicked on. Wall
    fades out with the zoom. Clouds move across the background until the
    zoom is completed.

---
+++ Assets/Scripts/CloudScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// If fade in has not completed
+		if (GameObject.Find ("Wall").GetComponent<WallScript>().fadeTimer > 0.0f)
+		{
+			// move right
+			Vector2 tempPos = transform.position;
+			tempPos.x += .009f;
+			transform.position = tempPos;
+
+			// once the cloud is off screen
+			if (transform.position.x > 13) {
+				// 1 in 1000 chance every frame that it will reset to the left
+				if (Random.Range(0, 1000) == 1){
+					tempPos.x = -13.0f;
+					transform.position = tempPos;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/WallScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallScript : MonoBehaviour {
+
+	public GameObject gameCamera;
+	bool fadeZoom = false;
+	public float fadeTimer = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// fade and zoom the camera in when the wall is clicked
+		if (fadeZoom && fadeTimer > 0.0f) {
+			// camera zoom
+			gameCamera.GetComponent<Camera>().orthographicSize -= .03f;
+
+			// camera drop to adjust for zoom
+			Vector3 tempPos = gameCamera.transform.position;
+			tempPos.y -= .03f;
+			gameCamera.transform.position = tempPos;
+
+			// fade wall
+			fadeTimer -= (Time.deltaTime * .5f);
+			gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, fadeTimer);
+		}
+	}
+
+	void OnMouseOver() {
+		// Initiate the fade in when the wall is moused over and clicked
+		if (Input.GetMouseButtonDown (0)) {
+			fadeZoom = true;
+		}
+	}
+}
