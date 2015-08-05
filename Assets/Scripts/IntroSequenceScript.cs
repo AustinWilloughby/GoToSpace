6e6b1cd85af6d3a34ae47d20dcbfac99a6859e73
Austin Willoughby <arw8020@rit.edu>
2015-08-04 21:18:42 -0400

    Stub Code For Intro Sequence
    Began scripting for the intro sequence. Will use timers and a switch to
    transition smoothly between different animation phases.

---
+++ Assets/Scripts/IntroSequenceScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroSequenceScript : MonoBehaviour
+{
+    //Fields
+    //Public
+    public float transitionTime;
+    public float logoTimer;
+
+    public GameObject logo;
+    public GameObject titleScreen;
+    public GameObject cameraDestination;
+
+    //Private
+    private int introStep = 0;
+    private float currentTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentTime = transitionTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentTime < 0)
+        {
+            introStep++;
+        }
+
+        if (introStep < 5)
+        {
+            switch (introStep)
+            {
+            }
+        }
+    }
+}
