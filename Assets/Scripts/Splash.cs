a3c40366300800c3e49507aaf30756d5efeb47c9
Ryan <rck1464@rit.edu>
2015-09-08 10:03:19 -0400

    Splash Screen
    Implemented the Splash Screen. Disabled the rotating sky test.

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Splash : MonoBehaviour {
+
+    public float waitTimer = 2.0f;
+    float fadeTimer1 = 1.0f;
+    float fadeTimer2 = 1.0f;
+    float fadeTimer3 = 1.0f;
+    float tipTimer = 0.1f;
+    float fadeTimer4 = 1.0f;
+
+    ArrayList starArray = new ArrayList();
+    public GameObject stars1 = null;
+    public GameObject stars2 = null;
+    public GameObject stars3 = null;
+    public GameObject stars4 = null;
+    public GameObject stars5 = null;
+
+    public GameObject fedora = null;
+    public GameObject mainStars = null;
+    public GameObject logo = null;
+
+	// Use this for initialization
+	void Start () {
+        starArray.Add(stars1);
+        starArray.Add(stars2);
+        starArray.Add(stars3);
+        starArray.Add(stars4);
+        starArray.Add(stars5);
+
+        fedora.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+        logo.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (waitTimer >= 0.0f)
+        {
+            waitTimer -= Time.deltaTime;
+        }
+        else if (fadeTimer1 >= 0.0f)
+        {
+            fadeTimer1 -= Time.deltaTime * .5f;
+
+            foreach (GameObject star in starArray)
+            {
+                star.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, fadeTimer1);
+            }
+        }
+        else if (fadeTimer2 >= 0.0f)
+        {
+            fadeTimer2 -= Time.deltaTime * .5f;
+
+            mainStars.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, fadeTimer2);
+            fedora.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1.0f - fadeTimer2);
+        }
+        else if (fadeTimer3 >= 0.0f)
+        {
+            fadeTimer3 -= Time.deltaTime * .5f;
+            logo.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1.0f - fadeTimer3);
+        }
+        else if (tipTimer >= 0.0f)
+        {
+            tipTimer -= Time.deltaTime * .5f;
+            if (!GameObject.Find("Fedora").GetComponent<Tip>().doTip) 
+            {
+                GameObject.Find("Fedora").GetComponent<Tip>().doTip = true;
+            }
+        }
+        else if (fadeTimer4 >= 0.0f)
+        {
+            fadeTimer4 -= Time.deltaTime * .5f;
+            fedora.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, fadeTimer4);
+            logo.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, fadeTimer4);
+        }
+        else
+        {
+            Application.LoadLevel("Title");
+        }
+	}
+}
---
+++ Assets/Scripts/Tip.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tip : MonoBehaviour {
+
+    public bool doTip = false;
+    float tipTimer = 0.1f;
+    float hangTimer = 0.03f;
+    float dir = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (doTip)
+        {
+            if (tipTimer >= 0.0f)
+            {
+                tipTimer -= Time.deltaTime * 0.5f;
+                gameObject.transform.Rotate(new Vector3(0, 0, dir));
+            }
+            else if (hangTimer > 0.0f)
+            {
+                hangTimer -= Time.deltaTime * 0.5f;
+            }
+            else if (dir > 0.0f)
+            {
+                tipTimer = .1f;
+                dir = -1.0f;
+            }
+        }
+	}
+}
