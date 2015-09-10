949c3fe61934fb850e6bf91edc1557bdcd1377a7
Austin Willoughby <arw8020@rit.edu>
2015-09-10 09:14:02 -0400

    Added Ryan's Changes
    Had to add ryans changes from my branch for some reason. Hopefully this
    fixes it?

---
+++ Assets/Scripts/IntroTransition.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroTransition : MonoBehaviour {
+
+    float timer = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        Vector3 temp = transform.position;
+        if (transform.position.y > -.37f)
+        {
+            temp.y -= .04f;
+            transform.position = temp;
+        }
+	}
+}
