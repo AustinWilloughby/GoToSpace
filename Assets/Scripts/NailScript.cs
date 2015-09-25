10fe087f758789011c59bdcb41887f1becf61468
rck1464 <rck1464@rit.edu>
2015-09-25 19:23:13 -0400

    Worked on MiniGame
    Can now identify correct and missed hits in console for individual nails

---
+++ Assets/Scripts/NailScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NailScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && gameObject.GetComponentsInChildren<ItemScript>()[1].mousedOver)
+        {
+            Debug.Log("Correct hit!");
+        }
+
+        else if (Input.GetMouseButtonDown(0) && GetComponent<ItemScript>().mousedOver)
+        {
+            Debug.Log("Missed hit!");
+        }
+	}
+}
