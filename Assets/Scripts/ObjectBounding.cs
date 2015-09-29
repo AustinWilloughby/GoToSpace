7399222757e7f0602924fa3905d68fbcb904afe9
Austin Willoughby <arw8020@rit.edu>
2015-09-29 08:40:29 -0400

    Object Bounding Implementation Prototyped
    Finished prototyping the object bounding script.  Currently it will
    contain any gameobjects (with trigger colliders) listed in
    boundedObjects within whatever object (with trigger collider) the script
    is attached to.

--- Assets/Scripts/ObjectBounding.cs
+++ Assets/Scripts/ObjectBounding.cs
@@ -15,14 +15,16 @@ public class ObjectBounding : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
     }
 
-    void OnColliderExit2D(Collider2D col)
+    void OnTriggerExit2D(Collider2D col)
     {
+        print(col);
         if (boundedObjects.Contains(col.gameObject))
         {
-            col.gameObject.transform.position = transform.position;
+            Vector2 fromObjToThis = (Vector2)transform.position - (Vector2)col.gameObject.transform.position;
+            fromObjToThis = fromObjToThis.normalized * col.bounds.size.magnitude;
+            col.gameObject.transform.position += (Vector3)fromObjToThis;
         }
     }
 }
--- Assets/Scripts/Splash Scripts/IntroTransition.cs
+++ Assets/Scripts/Splash Scripts/IntroTransition.cs
@@ -1,23 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IntroTransition : MonoBehaviour {
+public class IntroTransition : MonoBehaviour
+{
 
-    //float timer = 1.0f;
+    public float timeScale = 1;
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
-	void Update () 
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         Vector3 temp = transform.position;
-        if (transform.position.y > -.37f)
+        if (transform.position.y > -.39f)
         {
-            temp.y -= .04f;
+            temp.y -= .04f * timeScale;
             transform.position = temp;
+            if (transform.position.y < -.39f)
+            {
+                transform.position = new Vector3(transform.position.x, -.39f, transform.position.z);
+            }
         }
-	}
+    }
 }
