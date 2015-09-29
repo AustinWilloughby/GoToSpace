bdbf6551e88122a5c07b303348d9523b8d82a669
rck1464 <rck1464@rit.edu>
2015-09-28 21:15:16 -0400

    Finished Nail Minigame Prototype
    Can now record win and lose conditions and nailing movement
    placeholders.

---
+++ Assets/Scripts/Nail Minigame Scripts/NailGameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NailGameController : MonoBehaviour {
+
+    public bool completed;
+    public bool failed;
+    public GameObject nail1;
+    public GameObject nail2;
+    public GameObject nail3;
+    public GameObject nail4;
+
+	// Use this for initialization
+	void Start () {
+        completed = false;
+        failed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (nail1.GetComponent<NailScript>().hits > 3 &&
+            nail2.GetComponent<NailScript>().hits > 3 &&
+            nail3.GetComponent<NailScript>().hits > 3 &&
+            nail4.GetComponent<NailScript>().hits > 3)
+        {
+            completed = true;
+        }
+
+        if (nail1.GetComponent<NailScript>().misses > 4 ||
+            nail2.GetComponent<NailScript>().misses > 4 ||
+            nail3.GetComponent<NailScript>().misses > 4 ||
+            nail4.GetComponent<NailScript>().misses > 4)
+        {
+            failed = true;
+        }
+	}
+}
---
+++ Assets/Scripts/Nail Minigame Scripts/NailScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NailScript : MonoBehaviour {
+
+    public int hits;
+    public int misses;
+    float darkness;
+    Transform hitbox;
+    public Sprite sp1;
+    public Sprite sp2;
+    public Sprite sp3;
+    public Sprite sp4;
+
+	// Use this for initialization
+	void Start () {
+        hitbox = gameObject.transform.GetChild(0);
+        hits = 0;
+        misses = 0;
+        darkness = 0.5f;
+        GetComponent<SpriteRenderer>().color = new Color(darkness, darkness, darkness, 1.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0) && gameObject.GetComponentsInChildren<ItemScript>()[1].mousedOver)
+        {
+            switch (hits)
+            {
+                case 0:
+                    GetComponent<SpriteRenderer>().sprite = sp1;
+                    hitbox.transform.position -= Vector3.up * 0.13f;
+                    break;
+                case 1:
+                    GetComponent<SpriteRenderer>().sprite = sp2;
+                    hitbox.transform.position -= Vector3.up * 0.16f;
+                    break;
+                case 2:
+                    GetComponent<SpriteRenderer>().sprite = sp3;
+                    hitbox.transform.position -= Vector3.up * 0.15f;
+                    break;
+                case 3:
+                    GetComponent<SpriteRenderer>().sprite = sp4;
+                    break;
+                default:
+                    break;
+            }
+            hits++;
+            Debug.Log("Correct hit!");
+        }
+
+        else if (Input.GetMouseButtonDown(0) && GetComponent<ItemScript>().mousedOver)
+        {
+            Debug.Log("Missed hit!");
+            misses++;
+            darkness -= .1f;
+            GetComponent<SpriteRenderer>().color = new Color(darkness, darkness, darkness, 1.0f);
+        }
+	}
+}
