ef4c2ac6543342fee21314fff4f2cbae104e1bf3
Austin Willoughby <arw8020@rit.edu>
2015-09-29 01:34:48 -0400

    Frameworking for Object Bounding
    Beginning implementations for a system to keep movable objects on the
    screen, or atleast within a certain predefined shape.

---
+++ Assets/Scripts/ObjectBounding.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectBounding : MonoBehaviour
+{
+    public List<GameObject> boundedObjects;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnColliderExit2D(Collider2D col)
+    {
+        if (boundedObjects.Contains(col.gameObject))
+        {
+            col.gameObject.transform.position = transform.position;
+        }
+    }
+}
