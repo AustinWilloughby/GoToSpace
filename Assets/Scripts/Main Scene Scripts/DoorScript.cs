9b04469311f311b25a9b70439e02dd2ff30a77dc
Austin Willoughby <arw8020@rit.edu>
2015-10-06 09:00:48 -0400

    Player Effects Guy Movement
    The scripting that allows targeted movement for the guy has been
    implemented.  Also added a door that is now targetable, and once the guy
    reaches it, the street scene loads.

---
+++ Assets/Scripts/Main Scene Scripts/DoorScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorScript : MonoBehaviour
+{
+    private bool doorClicked = false;
+    private GameObject guy;
+
+    // Use this for initialization
+    void Start()
+    {
+        guy = GameObject.Find("Guy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doorClicked)
+        {
+            CheckForSceneTransition();
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            doorClicked = true;
+            guy.GetComponent<GuyBehavior>().MakeWalkToPosition(transform.position);
+        }
+    }
+
+    void CheckForSceneTransition()
+    {
+        if (Vector2.Distance((Vector2)guy.transform.position, (Vector2)transform.position) < .13f)
+        {
+            Application.LoadLevel("StreetScene");
+        }
+    }
+
+}
--- Assets/Scripts/Main Scene Scripts/GuyBehavior.cs
+++ Assets/Scripts/Main Scene Scripts/GuyBehavior.cs
@@ -1,25 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GuyBehavior : MonoBehaviour {
+public class GuyBehavior : MonoBehaviour
+{
 
     public float leftBound;
     public float rightBound;
-	public float walkSpeed;
+    public float walkSpeed;
     public bool isWalking;
     public bool facingRight;
 
-	// Use this for initialization
-	void Start () {
+    private bool WalkingToPosition;
+    private Vector2 initWalkPosition;
+    private Vector2 targetPosition;
+    private float walkTimer;
+    private float walkTime;
+
+    // Use this for initialization
+    void Start()
+    {
         leftBound = transform.position.x - 1.0f;
         rightBound = transform.position.x + 1.3f;
-		walkSpeed = .005f;
+        walkSpeed = .005f;
         isWalking = false;
         facingRight = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        WalkingToPosition = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!WalkingToPosition)
+        {
+            RandomWalking();
+        }
+        else
+        {
+            WalkToPosition();
+        }
+    }
+
+    //Initializes the guy walking to a position
+    public void MakeWalkToPosition(Vector3 targetPos)
+    {
+        WalkingToPosition = true;
+        initWalkPosition = (Vector2)transform.position;
+        targetPosition = (Vector2)targetPos;
+        walkTime = Vector2.Distance((Vector2)targetPos, (Vector2)transform.position) * 2;
+    }
+
+    //Handles moving the guy to target position
+    private void WalkToPosition()
+    {
+        if (Vector2.Distance((Vector2)transform.position, (Vector2)targetPosition) > .02f)
+        {
+            isWalking = true;
+            if (transform.position.x < targetPosition.x)
+            {
+                facingRight = true;
+            }
+            else
+            {
+                facingRight = false;
+            }
+            walkTimer += Time.deltaTime;
+            Vector3 newPos;
+            newPos.x = Mathf.Lerp(initWalkPosition.x, targetPosition.x, walkTimer / walkTime);
+            newPos.y = transform.position.y;
+            newPos.z = transform.position.z;
+            transform.position = newPos;
+        }
+        else
+        {
+            WalkingToPosition = false;
+        }
+    }
+
+    //Random walking when not targeted walking
+    private void RandomWalking()
+    {
         if (isWalking)
         {
             if (facingRight)
@@ -34,7 +93,7 @@ public class GuyBehavior : MonoBehaviour {
             else
             {
                 Vector3 temp = transform.position;
-				temp.x -= walkSpeed;
+                temp.x -= walkSpeed;
                 if (transform.position.x > leftBound)
                 {
                     transform.position = temp;
@@ -61,5 +120,5 @@ public class GuyBehavior : MonoBehaviour {
                 }
             }
         }
-	}
+    }
 }
