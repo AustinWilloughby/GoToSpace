e1a584e26f7fdf7c45c519cae8f8c6379533761e
rck1464 <rck1464@rit.edu>
2015-10-19 21:25:08 -0400

    Started Fuel Pouring Minigame
    Jet Fuel Can't Melt Steel Memes

---
+++ Assets/Scripts/Fueling/FuelScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelScript : MonoBehaviour {
+
+    float lifeSpan = 0.5f;
+    public bool destroyTrigger;
+    public bool hasCollided;
+
+	// Use this for initialization
+	void Start () {
+        destroyTrigger = false;
+        hasCollided = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (destroyTrigger)
+        {
+            lifeSpan -= Time.deltaTime;
+        }
+
+        if (lifeSpan <= 0)
+        {
+            GameObject.Find("FuelTankCollider").GetComponent<FuelTankScript>().intakeCount -= 1;
+            Destroy(gameObject);
+        }
+	}
+}
---
+++ Assets/Scripts/Fueling/FuelTankScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FuelTankScript : MonoBehaviour {
+
+    public Sprite deathSprite;
+    public int intakeCount;
+    GameObject deathScreen;
+
+	// Use this for initialization
+	void Start () {
+        intakeCount = 0;
+        deathScreen = GameObject.Find("ded");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "fuel" && !coll.gameObject.GetComponent<FuelScript>().hasCollided)
+        {
+            coll.gameObject.GetComponent<FuelScript>().hasCollided = true;
+            intakeCount += 1;
+            if (intakeCount > 15)
+            {
+                deathScreen.GetComponent<SpriteRenderer>().sprite = deathSprite;
+            }
+            coll.transform.gameObject.GetComponent<FuelScript>().destroyTrigger = true;
+        }
+    }
+}
