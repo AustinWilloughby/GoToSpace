f6fc24ea617129b1e4c079e69dd9325c5910e816
Austin Willoughby <arw8020@rit.edu>
2015-10-22 22:26:19 -0400

    Began StatusTracker Singleton Code
    Implemented a statustracking singleton that will be used to track the
    current progress of the game.

---
+++ Assets/Scripts/StatusTracker.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusTracker : MonoBehaviour
+{
+    #region Singleton Code
+    //Fields
+    private static StatusTracker instance = null;
+
+
+    //Properties
+    public static StatusTracker Instance
+    {
+        get { return instance; }
+    }
+
+
+    //Methods
+    void Awake() //Checks for other instance
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
