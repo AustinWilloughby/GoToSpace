a9bd6cba6db8759c2073db1a711074c212612b67
Ryan <rck1464@rit.edu>
2015-10-27 09:08:30 -0400

    Worked on welding
    Visual aspect is fully working, still need to decide on success fail
    conditions

---
+++ Assets/Scripts/Welding/WeldProgress.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeldProgress : MonoBehaviour {
+
+	GameObject[] weldTargets;
+	public int completed;
+	public int failed;
+
+	// Use this for initialization
+	void Start () {
+		weldTargets = GameObject.FindGameObjectsWithTag ("WeldTarget");
+		completed = 0;
+		failed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		checkProgress ();
+	}
+
+	void checkProgress()
+	{
+		completed = 0;
+		failed = 0;
+		for (int i = 0; i < weldTargets.Length; i++) 
+		{
+			if (weldTargets[i].GetComponent<WeldTarget>().completed)
+			{
+				completed++;
+			}
+
+			if (weldTargets[i].GetComponent<WeldTarget>().failed)
+			{
+				failed++;
+			}
+		}
+	}
+}
--- Assets/Scripts/Welding/WeldTarget.cs
+++ Assets/Scripts/Welding/WeldTarget.cs
@@ -4,25 +4,35 @@ using System.Collections;
 public class WeldTarget : MonoBehaviour {
 
     public float darkness;
+	public bool completed;
+	public bool failed;
+	GameObject torchPoint;
 
 	// Use this for initialization
 	void Start () {
         darkness = 1.0f;
+		torchPoint = GameObject.Find ("TorchPoint");
+		completed = false;
+		failed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		if (Vector2.Distance (torchPoint.transform.position, transform.position) < 0.7f && Input.GetMouseButton(0)) {
+			darkness -= .01f;
+			gameObject.GetComponent<SpriteRenderer> ().color = new Color (darkness, darkness, darkness, 1.0f);
+		}
 
-    void OnTriggerStay2D(Collider2D coll)
-    {
-        Debug.Log("Anything");
-        if (coll.gameObject.tag == "WeldTarget" && Input.GetMouseButton(0))
-        {
-            Destroy(coll.gameObject);
-            darkness -= .1f;
-            coll.gameObject.GetComponent<SpriteRenderer>().color = new Color(darkness, darkness, darkness, 1.0f);
-        }
-    }
+		if (!completed && !failed && darkness <= 0.5f) 
+		{
+			failed = false;
+			completed = true;
+		}
+
+		if (!failed && darkness <= 0.0f) 
+		{
+			completed = false;
+			failed = true;
+		}
+	}
 }
