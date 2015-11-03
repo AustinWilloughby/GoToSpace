feba7c021cfbd511cf04b16b3da9546926a0e051
rck1464 <rck1464@rit.edu>
2015-11-02 23:40:15 -0500

    Started Sawing Minigame
    Lots of placeholders in at the moment. Move the saw between two points
    without going out of bounds to avoid creating scratches.

---
+++ Assets/Scripts/Sawing/Saw.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saw : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 3.0f;
+        gameObject.transform.position = mousePos;
+	}
+}
---
+++ Assets/Scripts/Sawing/SawPoint.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawPoint : MonoBehaviour {
+
+    bool outOfBounds;
+    bool onScratch;
+    public GameObject scratch;
+    public float outOfBoundsTime;
+
+	// Use this for initialization
+	void Start () {
+        onScratch = false;
+        outOfBounds = false;
+        outOfBoundsTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (outOfBounds)
+        {
+            outOfBoundsTime += Time.deltaTime;
+        }
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("TriggerEnter");
+
+        if (other.tag == "Wall")
+        {
+            outOfBounds = true;
+        }
+
+        if (other.tag == "Scratch")
+        {
+            onScratch = true;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Wall")
+        {
+            if (!onScratch)
+            {
+                Vector3 temp = transform.position;
+                temp += new Vector3(0, 0, 1.0f);
+                Instantiate(scratch, temp, Quaternion.identity);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Wall")
+        {
+            outOfBounds = false;
+        }
+
+        if (other.tag == "Scratch")
+        {
+            onScratch = false;
+        }
+    }
+}
