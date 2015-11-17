6b860e0765b7fa5b5a895c0b6c9a944f82e4b1fb
rck1464 <rck1464@rit.edu>
2015-11-17 02:42:51 -0500

    Drill Game Started
    Bare Scene Added

---
+++ Assets/Scripts/Drill/Drill.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drill : MonoBehaviour {
+
+    public float intensity = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            Shake(intensity);
+        }
+	}
+
+    void Shake(float intensity)
+    {
+        Vector3 temp = new Vector3(Mathf.Sin(Time.time * intensity), transform.position.y, transform.position.z);
+        transform.position = temp;
+    }
+}
