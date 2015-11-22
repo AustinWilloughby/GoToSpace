0d5d21dea35392e1bff91a129430d62ddf4accf0
Austin Willoughby <arw8020@rit.edu>
2015-11-22 12:58:19 -0500

    Rocket Launch Sequence Started
    Began working on the rocket launch sequence.  Still have a lot of work
    to do, but it's a start.

---
+++ Assets/Scripts/BlastOff/LaunchParticle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchParticle : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private float lifeTimer;
+    // Use this for initialization
+    void Start()
+    {
+        Color particleColor = GetComponent<SpriteRenderer>().color;
+        particleColor.g = Random.Range(70, 256);
+        GetComponent<SpriteRenderer>().color = particleColor;
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        Vector3 vel = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-0.5f, -1.0f), 0);
+        rigidBody.velocity = vel;
+        lifeTimer = Random.Range(.5f, 1.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTimer -= Time.deltaTime;
+        if(lifeTimer <= 0)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Ground")
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/BlastOff/LaunchScript.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaunchScript : MonoBehaviour
+{
+    public enum launchStage
+    {
+        Stationary,
+        FiringUp,
+        LiftOff,
+        Cruise
+    };
+
+    public GameObject rocket;
+    public RocketThruster rocketThrust;
+    public float sequenceTime;
+    private float sequenceTimer;
+    public launchStage currentStage;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentStage = launchStage.Stationary;
+        sequenceTimer = sequenceTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sequenceTimer -= Time.deltaTime;
+        if(sequenceTimer > (sequenceTime - 2))
+        {
+            currentStage = launchStage.Stationary;
+        }
+        else if(sequenceTimer > (sequenceTime - 4) && sequenceTimer < (sequenceTime - 2))
+        {
+            currentStage = launchStage.FiringUp;
+            //gameObject.GetComponent<Camera>().orthographicSize -= .01f * Time.deltaTime;
+        }
+        else if(sequenceTimer > (sequenceTime - 7) && sequenceTimer < (sequenceTime - 4))
+        {
+            currentStage = launchStage.LiftOff;
+        }
+        else
+        {
+            currentStage = launchStage.Cruise;
+        }
+
+        Vector3 rocketPos = rocket.transform.position;
+        switch(currentStage)
+        {
+            case launchStage.Stationary:
+                rocketThrust.particleInterval = .5f;
+                break;
+
+
+            case launchStage.FiringUp:
+                rocketThrust.particleInterval = .1f;
+                rocketPos.y += .1f * Time.deltaTime * (10.0f / sequenceTimer);
+                rocket.transform.position = rocketPos;
+
+                rocket.transform.Rotate(0, 0, Random.Range(-.25f, .25f));
+                break;
+
+
+            case launchStage.LiftOff:
+                rocketThrust.particleInterval = .02f;
+
+                rocketPos.y += 3.0f * Time.deltaTime;
+                rocket.transform.position = rocketPos;
+                if (rocket.transform.rotation.z < -1.0f || rocket.transform.rotation.z > 359.0f)
+                {
+                    rocket.transform.Rotate(0, 0, Random.Range(0.0f, .3f));
+                }
+                else if(rocket.transform.rotation.z > 1.0)
+                {
+                    rocket.transform.Rotate(0, 0, Random.Range(-0.3f, 0.0f));
+                }
+                else
+                {
+                    rocket.transform.Rotate(0, 0, Random.Range(-.5f, .5f));
+                }
+                break;
+
+
+                //Default is standin for cruise
+            default:
+                if (rocket.transform.rotation.z < - .03f || rocket.transform.rotation.z > 360.0f - .03f)
+                {
+                    rocket.transform.Rotate(0, 0, Random.Range(0.0f, .01f));
+                }
+                else if (rocket.transform.rotation.z > .03f)
+                {
+                    rocket.transform.Rotate(0, 0, Random.Range(-0.01f, 0.0f));
+                }
+                break;
+        }
+    }
+}
+
