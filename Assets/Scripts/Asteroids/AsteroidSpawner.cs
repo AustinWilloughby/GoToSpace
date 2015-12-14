624b15909da905409e50d53c8824ae17d33532f6
Austin Willoughby <arw8020@rit.edu>
2015-12-14 15:59:52 -0500

    Asteroids Work
    Beginning Implementation for asteroids mini game. All thats left is
    asteroid spawning and collisions.

---
+++ Assets/Scripts/Asteroids/AsteroidSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	private GameObject[] asteroids;
+	private float spawnTimer;
+	public GameObject ship;
+
+	// Use this for initialization
+	void Start () {
+		spawnTimer = 0;
+		ship = GameObject.Find ("ship");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnTimer <= 0) {
+			spawnTimer = Random.Range (.3f, .6f);
+			GameObject asteroid = asteroids[Random.Range(0, asteroids.Length)];
+
+			Vector2 position = Random.insideUnitCircle * 12;
+			Vector3 startPos = (Vector3)position + ship.transform.position;
+
+			GameObject.Instantiate(asteroid, startPos, Quaternion.identity);
+
+		} 
+
+		else {
+			spawnTimer -= Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Scripts/Asteroids/TouchTrack.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchTrack : MonoBehaviour {
+
+	public Vector3 targLocation;
+	private bool fired = true;
+	public Ship spaceShip;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x != targLocation.x || transform.position.y != targLocation.y) {
+			Vector3 moveTo = targLocation - transform.position;
+			moveTo *= Time.deltaTime * 5;
+			moveTo += transform.position;
+			transform.position = moveTo;
+
+			if(Mathf.Abs(transform.position.x - targLocation.x) < .5f && Mathf.Abs(transform.position.y - targLocation.y) < .5f)
+			{
+				if(fired == false)
+				{
+					fired = true;
+					spaceShip.Fire();
+				}
+				else{
+					if (Input.GetMouseButtonDown (0)) {
+						Vector2 mousePos = Input.mousePosition;
+						targLocation = Camera.main.ScreenToWorldPoint(mousePos);
+						fired = false;
+					}
+				}
+			}
+		}
+	}
+}
