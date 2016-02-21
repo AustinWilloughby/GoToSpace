ced849316271c4a1474a2c2d526d758cbccb7f93
Austin Willoughby <arw8020@rit.edu>
2016-02-21 11:39:19 -0500

    Added Ambient Noise
    Added ambient noise for all required scenes. Also imported SFX audio
    assets to be plugged in.

---
+++ Assets/Scripts/Globals/Audio/AmbienceSingleton.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AmbienceSingleton : MonoBehaviour
+{
+    #region Singleton Code
+    //Fields
+    private static AmbienceSingleton instance = null;
+
+
+    //Properties
+    public static AmbienceSingleton Instance
+    {
+        get { return instance; }
+    }
+
+
+    //Methods
+    void Awake() //Checks for other instance
+    {
+        lowPass = this.gameObject.GetComponent<AudioLowPassFilter>();
+        source = this.gameObject.GetComponent<AudioSource>();
+        source.Stop();
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    public AudioClip[] ambience;
+    private AudioLowPassFilter lowPass;
+    private AudioSource source;
+    private int currentIndex;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentIndex = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckForUpdate();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        switch(level)
+        {
+            case 3: //Street
+            case 4: //Workshop
+            case 5: //PaintShop
+            case 6: //HardwareShop
+            case 10: //Yard
+                currentIndex = 0;
+                CheckForUpdate();
+                break;
+                
+            case 12: //InsideShip
+                currentIndex = 1;
+                CheckForUpdate();
+                break;
+
+            default:
+                source.Stop();
+                break;
+        }
+
+        switch(level)
+        {
+            case 4: //Workshop
+            case 5: //PaintShop
+            case 6: //HardwareShop
+                lowPass.enabled = true;
+                source.volume = .7f;
+                break;
+
+            case 12: //InsideShip
+                lowPass.enabled = false;
+                source.volume = .3f;
+                break;
+
+            default:
+                lowPass.enabled = false;
+                source.volume = .6f;
+                break;
+        }
+
+    }
+
+    private void CheckForUpdate()
+    {
+        if (currentIndex >= 0)
+        {
+            if (source.clip != ambience[currentIndex])
+            {
+                source.Stop();
+                source.clip = ambience[currentIndex % ambience.Length];
+                source.Play();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Globals/Audio/FXHandler.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FXHandler : MonoBehaviour
+{
+    private AudioSource source;
+
+    public AudioClip[] soundFX;
+
+    private MusicSingleton music;
+    // Use this for initialization
+    void Start()
+    {
+        source = this.GetComponent<AudioSource>();
+        try
+        {
+            source.volume = MusicSingleton.Instance.volume;
+        }
+        catch { }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckVolume();
+    }
+
+    public void PlayEffect(string sound)
+    {
+        int index = -1;
+        for(int i = 0; i < soundFX.Length; i++)
+        {
+            if(soundFX[i].name == sound)
+            {
+                index = i;
+            }
+        }
+        if(index > 0 && index < soundFX.Length)
+        {
+            source.clip = soundFX[index];
+        }
+        else
+        {
+            Debug.Log("Effect not found");
+        }
+    }
+
+    private void CheckVolume()
+    {
+        try
+        {
+            if (source.volume != MusicSingleton.Instance.volume)
+            {
+                source.volume = MusicSingleton.Instance.volume;
+            }
+        }
+        catch { }
+    }
+}
